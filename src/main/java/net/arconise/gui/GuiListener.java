cf99f32403d53a1e5e4c8da837e17c141f6dd480
Avelify <pazoree@gmail.com>
2024-03-17 10:53:45 -0400

    asd

--- src/main/java/net/arconise/gui/GuiListener.java
+++ src/main/java/net/arconise/gui/GuiListener.java
@@ -2,6 +2,7 @@ package net.arconise.gui;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,13 +16,15 @@ public class GuiListener implements Listener {
         Player p = (Player) e.getWhoClicked();
 
         if (e.getView().getTitle().equalsIgnoreCase("Test")) {
-        } else if (e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GREEN + "Diamond_Sword")) {
-            p.getInventory().addItem(new ItemStack(Material.DIAMOND_SWORD, 230));
+
+        }
+
+        if (e.getCurrentItem().getType() == Material.DIAMOND_SWORD){
+            p.getInventory().addItem(new ItemStack(Material.DIAMOND_SWORD, 74));
             p.closeInventory();
             e.setCancelled(true);
         }
         e.setCancelled(true);
-
     }
 
 }
